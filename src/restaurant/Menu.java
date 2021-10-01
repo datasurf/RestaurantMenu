4f3880b3661c5517c9ed2049ca88c059589d1ed2
claudiolespier <clespier@gmail.com>
2021-09-30 22:03:48 -0400

    Added setters and getters.

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -11,40 +11,44 @@ public class Menu {
     private HashMap<String, ArrayList<MenuItem>> items;
     private Double price;
 
-    public void setCreatedOn(Date createdOn){
-        this.createdOn=createdOn;
-    }
+
     public Date getCreatedOn() {
         return createdOn;
     }
-    public void setNewItem(Boolean newItem){
-        this.newItem=newItem;
-    }
+
     public Boolean getNewItem() {
         return newItem;
     }
-    public void setItems(HashMap<String, ArrayList<MenuItem>> items) {
-        this.items = items;
-    }
-    public HashMap<String, ArrayList<MenuItem>> getItems() {
-        return items;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setCategories(String[] categories) {
-        this.categories = categories;
-    }
+
     public String[] getCategories() {
         return categories;
     }
-    public Menu() {
 
+    public HashMap<String, ArrayList<MenuItem>> getItems() {
+        return items;
+    }
 
+    public Double getPrice() {
+        return price;
+    }
 
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
 
+    public void setNewItem(Boolean newItem) {
+        this.newItem = newItem;
+    }
+
+    public void setCategories(String[] categories) {
+        this.categories = categories;
+    }
+
+    public void setItems(HashMap<String, ArrayList<MenuItem>> items) {
+        this.items = items;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -14,38 +14,46 @@ public class MenuItem {
         this.description = description;
         this.category = category;
         this.isNew = true;
+        private Date createdOn;
     }
 
-    public boolean getIsNew() {
-        return isNew;
-    }
-
-    private void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
     public String getDescription() {
         return description;
     }
 
-    private void setDescription(String description) {
-        this.description = description;
-    }
     public String getCategory() {
         return category;
     }
-    private void setCategory(String category) {
-        this.category = category;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-    public void setNew(Boolean aNew) {
-        isNew = aNew;
-    }
+
     public Boolean getNew() {
         return isNew;
     }
-    public void setCreatedOn(Date createdOn) {
-        this.createdOn = createdOn;
+
+    public Date getCreatedOn() {
+        return createdOn;
     }
 }
